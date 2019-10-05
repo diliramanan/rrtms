84d22e09c6f4def629ee2b6859487568dcc20459
Dilip Ramanan <diliramanan@gmail.com>
2019-10-05 13:14:52 +0530

    commit

--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -5,7 +5,7 @@ const data = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
     {
-      label: 'My First dataset',label: 'My First dataset',
+      label: 'My First dataset',
       fill: false,
       lineTension: 0.1,
       backgroundColor: 'rgba(75,192,192,0.4)',
